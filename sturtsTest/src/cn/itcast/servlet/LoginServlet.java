ea5a3015af52a557f9786d6554f25e9444b6d65a
王冀琛 <1843600144@qq.com>
2017-03-26 18:24:48 +0800

    Create LoginServlet.java

---
+++ sturtsTest/src/cn/itcast/servlet/LoginServlet.java
@@ -0,0 +1,55 @@
+package cn.itcast.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import cn.itcast.entity.User;
+import cn.itcast.framework.action.LoginAction;
+import cn.itcast.service.UserService;
+
+//控制器
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//创建Action
+		LoginAction loginAction = new LoginAction();
+		
+		//调用登陆方法
+		Object uri = loginAction.login(request, response);
+		
+		//跳转
+		if(uri instanceof String){
+			response.sendRedirect(request.getContextPath()+uri.toString());
+		}else{
+			((RequestDispatcher)uri).forward(request, response);
+		}
+			
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
